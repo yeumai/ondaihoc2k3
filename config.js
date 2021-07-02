37e85960f1fbad7f1529bb67f844624f3ee7c04b
yeumai <86833335+yeumai@users.noreply.github.com>
2021-07-02 19:42:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const CONFIG = {
-    introTitle: 'Maiiii à!',
-    introDesc: 'Yêu Mai Nhiềuuuuu`,
+    introTitle: 'Maiii à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao Mai cứ đi nhầm đường
+    Lạc vào tim Tiến lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Tiến đã yêu Mai từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Tiến đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Tiến <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Tiến lại yêu Mai <3',
-    btnReply: 'Gửi cho meee <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Mai lại yêu Tiến <3',
+    btnReply: 'Gửi cho Meee <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu Mai nhiều nhiều 😘😘',
-    messDesc: 'vậy là Mai là người yêu mình rồi nha.',
+    messDesc: 'Giờ mai là người yêu mình rồi nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/tienzk3dzz'
 }
