2a4f3e891cab8d7d614ecd5dcf3a4f959b1e31e9
yeumai <86833335+yeumai@users.noreply.github.com>
2021-07-02 20:08:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Maiii à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao Mai cứ đi nhầm đường
-    Lạc vào tim Tiến lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích Anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Mai lại yêu Tiến <3',
-    btnReply: 'Gửi cho Meee <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu Mai nhiều nhiều 😘😘',
-    messDesc: 'Giờ mai là người yêu mình rồi nhé.',
+    introTitle: 'Mai ơi🥰!',
+    introDesc: `Trời sinh voi trời sinh cỏ
+    Em là cà rốt anh là con thỏ
+    Anh say yes Em say ỏooo`,
+    btnIntro: 'I Love You',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là Yêu Tiến <33',
+    btnNo: ' hông à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà mai chỉ yêu mỗi tiến <3',
+    btnReply: 'Gửi tặng Mai <3',
+    reply: 'Yêu thì yêu mà không yêu thì phải yêu <33333333',
+    mess: 'Mai biết mà 🥰. Yêu Mai nhiều nhiều 😘😘',
+    messDesc: 'giờ thì mai là người yêu mình rồi nha💪💪.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/tienzk3dzz'
-}
+    messLink: 'https://www.facebook.com/tienzk3dzz
