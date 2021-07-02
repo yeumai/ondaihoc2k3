60052e3fd614664c385d8e250cb47aadccef5c42
yeumai <86833335+yeumai@users.noreply.github.com>
2021-07-02 19:44:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích Tiến <33',
+    btnYes: 'Vẫn cứ là thích Anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao Mai lại yêu Tiến <3',
     btnReply: 'Gửi cho Meee <3',
